77193e5cee9b33b55e2b7e2aa0ee18910c675e2f
Dutiyesh Salunkhe <dutiyesh@gmail.com>
2019-09-14 16:49:36 +0530

    Improve helper message
    - Provide example in helper message
    
    - Update --help command message

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ const program = new commander.Command('chrome-extension-cli')
   .action(name => {
     projectName = name;
   })
+  .on('--help', () => {
+    console.log(`    Only <project-directory> is required.`);
+  })
   .parse(process.argv);
 
 // Exit from the process if no project name is provided
 if (typeof projectName === 'undefined') {
-  console.error('Please specify the project directory');
+  console.error('Please specify the project directory:');
+  console.log(`  ${program.name()} <project-directory>`);
+  console.log();
+  console.log('For example:');
+  console.log(`  ${program.name()} my-extension`);
+  console.log();
+  console.log(`Run ${program.name()} --help to see all options.`);
   process.exit(1);
 }
 

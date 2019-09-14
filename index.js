4aae0b9496939eed4a805f2012778b774576c893
Dutiyesh Salunkhe <dutiyesh@gmail.com>
2019-09-14 16:30:40 +0530

    Integrate commander

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 
 'use strict';
 
+const commander = require('commander');
+
+const program = new commander.Command('chrome-extension-cli')
+  .version('0.1.0')
+  .parse(process.argv);
+
 console.log('The CLI for your next Chrome Extension. 🚀');

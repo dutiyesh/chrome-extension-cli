eddaffc68bccd809998b3446e438ca6305cb2144
Dutiyesh Salunkhe <dutiyesh@gmail.com>
2019-09-14 16:36:52 +0530

    Add project-directory argument

--- index.js
+++ index.js
@@ -4,8 +4,16 @@
 
 const commander = require('commander');
 
+let projectName;
+
 const program = new commander.Command('chrome-extension-cli')
   .version('0.1.0')
+  .arguments('<project-directory>')
+  .usage('<project-directory> [options]')
+  .action(name => {
+    projectName = name;
+  })
   .parse(process.argv);
 
 console.log('The CLI for your next Chrome Extension. 🚀');
+console.log(`Poject name: ${projectName}`);

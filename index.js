9e06e929acbf12143b1dd7dfa394a125ac231612
Andras <andras@cabsolutely.co>
2019-11-02 19:28:53 +0100

    Update index.js: usign RegExp.test() instead of String.match()

--- index.js
+++ index.js
@@ -182,8 +182,8 @@ function createExtension(name, { overridePage, devtools, crossBrowser }) {
   const files = [];
   fs.copySync(path.resolve(__dirname, 'templates', templateName), root, {
     filter: filename => {
-      if(!filename.match(/\.js$/)) return true; // copy all files that aren't .js files
-      if(!!filename.match(/^webpack\.config\.js$/)) return true; // explcitly copy webpack.config.js all the time
+      if(!/\.js$/.test(filename)) return true; // copy all files that aren't .js files
+      if(/^webpack\.config\.js$/.test(filename)) return true; // explcitly copy webpack.config.js all the time
       if(!crossBrowser && filename.indexOf('.nocrossbrowser') > -1) {
         files.push(filename);
         return true;

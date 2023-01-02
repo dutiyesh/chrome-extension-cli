49772ecb20dffc3dba908b7fa0f972ca63c3294e
Internal Stability <117607574+internalstability@users.noreply.github.com>
2023-01-02 22:53:39 +0900

    Use base instead of name of parsed path

--- templates/shared/pack.js
+++ templates/shared/pack.js
@@ -2,11 +2,11 @@ const { readFileSync } = require('fs');
 const { parse, resolve } = require('path');
 const AdmZip = require('adm-zip');
 
-const { name } = parse(__dirname);
+const { base } = parse(__dirname);
 const { version } = JSON.parse(
   readFileSync(resolve(__dirname, 'build', 'manifest.json'), 'utf8')
 );
 
 const zip = new AdmZip();
 zip.addLocalFolder('build');
-zip.writeZip(`${name}-v${version}.zip`);
+zip.writeZip(`${base}-v${version}.zip`);
